78c067e6c1e25965bc95a0c744913d4252d3c4b3
Alan <alan_ps@hotmail.com>
2024-02-27 21:04:23 -0300

    Split Schedule component finished.

--- src/SplitSchedule.js
+++ src/SplitSchedule.js
@@ -1,21 +1,41 @@
 import React, { useState } from "react";
-import "./ElapseDataChecking.css";
+import "./SplitSchedule.css";
 
-function ElapseDataChecking() {
+function SplitSchedule() {
+  const [choice, setChoice] = useState("");
+
   return (
-    <div className="elapse-wrapper">
-      <div className="elapse-header">
+    <div className="split-wrapper">
+      <div className="split-header">
         <div className="component-title">
-          <p>Elapse Data Checking</p>
+          <p>Split schedule using social distancing?</p>
         </div>
         <div className="component-title">
-          <p style={{ marginTop: "-8px", color: "#32AB89" }}>
-            No Elapsed Dates!
-          </p>
+          <div className="split-choice">
+            <label>
+              <input
+                type="radio"
+                value="yes"
+                checked={choice === "yes"}
+                onChange={(e) => setChoice(e.target.value)}
+              />{" "}
+              <span className="radio-text">Yes</span>
+            </label>
+            <label style={{ marginLeft: "20px" }}>
+              {" "}
+              <input
+                type="radio"
+                value="no"
+                checked={choice === "no"}
+                onChange={(e) => setChoice(e.target.value)}
+              />{" "}
+              <span className="radio-text">No</span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default ElapseDataChecking;
+export default SplitSchedule;
